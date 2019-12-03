a0785efad3ed086c83b43d07542711da633d8775
Igor Kamyshev <igor@kamyshev.me>
2019-12-03 17:00:17 +0300

    remove unused types

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,6 @@
 declare module 'plural-ru' {
   type ThreeForms = [string, string, string];
   type TwoForms = [string, string];
-  type Forms = TwoForms | ThreeForms;
 
   const noun: (num: number, ...forms: TwoForms | ThreeForms) => string;
   const verb: (num: number, ...forms: ThreeForms) => string;
